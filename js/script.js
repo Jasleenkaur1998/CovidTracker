722b9dfcccdc8f0979648b3c6e8fc3df29bc8422
Jasleen Kaur <jasleen2357@gmail.com>
2023-09-26 16:26:09 -0700

    Refs #11: Updated the chart component

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 const apiUrl = 'https://api.opencovid.ca/summary?geo=pt&fill=true&version=true&pt_names=short&hr_names=hruid&fmt=json'
 const apiData = document.getElementById('api-data')
+const chartData = document.getElementById('chart')
 
 let covidData = []
 
@@ -10,6 +11,7 @@ const getData = async () => {
             covidData = data.data
             console.log(covidData)
             displayData(data.data)
+            displayChart()
         })
         .catch(err => console.error(err))
 }
@@ -24,3 +26,43 @@ const displayData = () => {
         apiData.appendChild(p)
     })
 }
+
+const displayChart = () => {
+    var options = {
+        series: [{
+            name: "Cases",
+            data: [ 634149, 404182, 157105, 91761, 55896, 147179, 11511, 3531, 1637787, 57415, 1360972, 155417, 5588]
+        }],
+        chart: {
+            height: 350,
+            type: 'line',
+            zoom: {
+                enabled: false
+            }
+        },
+        dataLabels: {
+            enabled: false
+        },
+        stroke: {
+            curve: 'straight'
+        },
+        title: {
+            text: 'Cases Trends by Month',
+            align: 'left'
+        },
+        grid: {
+            row: {
+                colors: ['#f3f3f3', 'transparent'], 
+            },
+        },
+        xaxis: {
+            categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct','Nov','Dec'],
+        }
+    };
+
+    var chart = new ApexCharts(chartData, options);
+    chart.render();
+
+}
+
+
